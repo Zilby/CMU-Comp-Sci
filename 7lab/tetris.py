12b8ebd34e3ca0ffb17e9d95b8cc73993db3ad93
Zilby <azilby@gmail.com>
2014-08-02 22:03:42 -0400

    added tetris, working on rotation

--- 7lab/tetris.py
+++ 7lab/tetris.py
@@ -13,8 +13,9 @@ def run(rows,cols):
     canvas.data=Struct()
     canvas.data.rows=rows
     canvas.data.cols=cols
+    root.bind("<Key>", keyPressed)
     init()
-    redrawAll()
+    timerFired()
     root.mainloop()
 
 def init():
@@ -45,6 +46,7 @@ def init():
     canvas.data.fallingPieceRow=0
     canvas.data.fallingPieceCol=0
     canvas.data.fallingPieceCols=0
+    canvas.data.fallingPieceRows=0
     newFallingPiece()
 
 def newFallingPiece():
@@ -52,13 +54,19 @@ def newFallingPiece():
     canvas.data.fallingPiece=canvas.data.tetrisPieces[indexNum]
     canvas.data.fallingPieceColor=canvas.data.tetrisPieceColors[indexNum]
     canvas.data.fallingPieceRow=0
+    canvas.data.fallingPieceRows=len(canvas.data.fallingPiece)
     canvas.data.fallingPieceCols=len(canvas.data.fallingPiece[0])
     canvas.data.fallingPieceCol=(canvas.data.cols/2)-(canvas.data.fallingPieceCols/2)
 
 def redrawAll():
+    canvas.delete(ALL)
     drawGame()
     drawFallingPiece()
-    canvas.after(50,redrawAll)
+    #newFallingPiece()
+
+def timerFired():
+    redrawAll()
+    canvas.after(50,timerFired)
 
 def drawGame():
     '''
@@ -74,6 +82,7 @@ def drawGame():
 def drawBoard():
     for col in range(canvas.data.cols):
         for row in range(canvas.data.rows):
+            #canvas.board[row][col]="blue"
             drawCell(row,col,canvas,canvas.board[row][col])
 
 def drawFallingPiece():
@@ -83,6 +92,7 @@ def drawFallingPiece():
         for block in vList:
             if(block==True):
                 drawCell(row,col,canvas,canvas.data.fallingPieceColor)
+                #canvas.board[row][col]=canvas.data.fallingPieceColor
             col+=1
         col=canvas.data.fallingPieceCol
         row+=1
@@ -100,6 +110,53 @@ def drawCell(row,col,canvas,color):
     y2 = y1 + canvas.data.cellDimension-10
     canvas.create_rectangle(x1,y1,x2,y2,fill=color,width=0)
 
+def moveFallingPiece(drow,dcol):
+    canvas.data.fallingPieceRow=canvas.data.fallingPieceRow+drow
+    canvas.data.fallingPieceCol=canvas.data.fallingPieceCol+dcol
+    if(fallingPieceIsLegal()==False):
+        canvas.data.fallingPieceRow=canvas.data.fallingPieceRow-drow
+        canvas.data.fallingPieceCol=canvas.data.fallingPieceCol-dcol
+    
+
+def fallingPieceIsLegal():
+    row=canvas.data.fallingPieceRow
+    col=canvas.data.fallingPieceCol
+    for vList in canvas.data.fallingPiece:
+        for block in vList:
+            if(block==True):
+                if(row>canvas.data.rows-1 or row<0 or
+                   col>canvas.data.cols-1 or col<0 or
+                   canvas.board[row][col]!="blue"):
+                    return False
+            col+=1
+        col=canvas.data.fallingPieceCol
+        row+=1
+    return True
+
+def rotateFallingPiece():
+    tempPiece=canvas.data.fallingPiece
+    tempDimensions=[canvas.data.fallingPieceRows,canvas.data.fallingPieceCols]
+    tempLocation=[canvas.data.fallingPieceRow,canvas.data.fallingPieceCol]
+    print "Location: Row: "+str(tempLocation[0])+" Col: "+str(tempLocation[1])
+    #print "Dimensions: Rows: "+str(tempDimensions[0])+" Cols: "+str(tempDimensions[1])
+    canvas.data.fallingPieceRows=tempDimensions[1]
+    canvas.data.fallingPieceCols=tempDimensions[0]
+    canvas.data.fallingPieceCol+=tempDimensions[1]-(tempDimensions[0]/2)
+    canvas.data.fallingPiece=[]
+    count=0
+    while(count<tempDimensions[1]):
+        x=[]
+        for each in tempPiece:
+            x+=[each[count]]
+        canvas.data.fallingPiece+=[x]
+        count+=1
+    if(fallingPieceIsLegal==False):
+        canvas.data.fallingPiece=tempPiece
+        canvas.data.fallingPieceRows=tempDimensions[0]
+        canvas.data.fallingPieceCols=tempDimensions[1]
+        canvas.data.fallingPieceRow=tempLocation[0]
+        canvas.data.fallingPieceCol=tempLocation[1]
+
 def printBoard():
     print "["
     for col in canvas.board:
@@ -110,11 +167,22 @@ def printBoard():
         print "\n"
     print "]"
 
+
 def keyPressed(event):
-  # for now, for testing purposes, just choose a new falling piece
-  # whenever ANY key is pressed!
-  canvas = event.widget.canvas
-  newFallingPiece()
-  redrawAll()
+    if(event.keysym=='Left'):
+        moveFallingPiece(0,-1)
+    elif(event.keysym=='Right'):
+        moveFallingPiece(0,1)
+    elif(event.keysym=='Down'):
+        moveFallingPiece(1,0)
+    elif(event.char==' '):
+        rotateFallingPiece()
+    #elif(event.keysym=='Up'):
+        #moveFallingPiece(-1,0)
+    else:   
+        newFallingPiece()
+        redrawAll()
+        #printBoard()
+
 
 run(15,10)
